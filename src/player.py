66ae007cb06c4be96899e0d98ba4f7eaec16f0e2
andresafira <andresafira2004@gmail.com>
2024-03-15 17:47:58 -0300

    feat: implement basic player features

--- src/player.py
+++ src/player.py
@@ -1,11 +1,15 @@
 from utils import Vector
-
+from typing import Union
 
 
 class Player:
     def __init__(self, initial_pos: Vector):
         self.position = initial_pos
         self.velocity = Vector(0, 0)
-    
+        self.alive = True
+
+        self.gh_position: Union[None, Vector] = None
+        self.gh_attached: bool = False
+
     def move(self):
         self.position += self.velocity
---
+++ src/player_constants.py
@@ -0,0 +1,10 @@
+# GH == Grappling Hook
+# Todos os valores a seguir foram carteados
+
+GH_MAX_LENGTH: float = 6.9
+GH_MAX_THRUST: float = 6.9
+
+PLAYER_WIDHT: float = 6.9
+PLAYER_HEIGHT: float = 6.9
+
+
