ba0e79909ea1467863cc670878d13c0dd8972937
andresafira <andresafira2004@gmail.com>
2024-03-25 17:11:56 -0300

    refactor: adjust player constants

--- src/constants/player_constants.py
+++ src/constants/player_constants.py
@@ -8,11 +8,11 @@ PLAYER_WIDHT: float = 6.9
 PLAYER_HEIGHT: float = 6.9
 
 # Movement Constants
-ACC_KP: float = 5 # Proportionality constant for horizontal movement: acc = ACC_KP*(target_speed - speed)
+ACC_KP: float = 8 # Proportionality constant for horizontal movement: acc = ACC_KP*(target_speed - speed)
 MAX_HORIZONTAL_SPEED: float = 200
 MAX_JUMP_SPEED: float = 200
 MAX_FALL_SPEED: float = 600
 
-BASE_G_VALUE: float = -400  # Basic acceleration value (remember to be negative)
-FALL_G_MULTIPLIER: float = 1.5  # Gravity multiplier while falling (intended to be greater than 1)
-JUMP_G_CONTROL: float = 0.5  # Gravity multiplier (intended to be less than 1)
+BASE_G_VALUE: float = -600  # Basic acceleration value (remember to be negative)
+FALL_G_MULTIPLIER: float = 1.2  # Gravity multiplier while falling (intended to be greater than 1)
+JUMP_G_CONTROL: float = 0.4  # Gravity multiplier (intended to be less than 1)
