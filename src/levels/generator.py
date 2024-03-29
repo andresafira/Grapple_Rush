42103bce846d9bdf5933961a36c79f716606b9ba
andresafira <andresafira2004@gmail.com>
2024-03-29 12:08:16 -0300

    refactor: change test level design

--- src/levels/generator.py
+++ src/levels/generator.py
@@ -3,8 +3,8 @@ import json
 
 map_ = [[0 if i < 20 else 1 for j in range(50)] for i in range(30)]
 
-for i in range(5):
-        map_[20 - i][5 + 2*i] = 1
+for i in range(6):
+        map_[20 - i][5 + 3*i] = 1
 map_[19][24] = 1
 map_[18][24] = 1
 map_[17][24] = 1
