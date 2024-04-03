1c9d30282c3546e41ba06e2097ecd2f1705a0724
Candango <gui.22103@gmail.com>
2024-04-03 19:44:00 -0300

    Added background images
    Function for drawing background was implemented. Also added a function for the level editor

--- src/engine.py
+++ src/engine.py
@@ -3,7 +3,7 @@ from typing import Union
 
 from level import Level
 from player import Player
-from constants.game_constants import FPS, WIDTH, HEIGHT
+from constants.game_constants import FPS, WIDTH, HEIGHT, ROWS, COLS, TILE_WIDTH, TILE_HEIGHT, SIDE_MARGIN, LOWER_MARGIN
 
 # pygame libraries management
 import pygame
@@ -17,6 +17,7 @@ class GameState(Enum):
     MENU = 0
     GAME = 1
     OPTIONS = 2
+    EDITOR = 3
     # add other states
 
 
@@ -25,13 +26,19 @@ class Engine:
         pygame.init()
         pygame.display.set_caption("Grappler Rush")
         self.screen = pygame.display.set_mode((WIDTH, HEIGHT))
-
-        self.state: GameState = GameState.GAME
+        self.resized = False
+        self.state: GameState = GameState.EDITOR
         self.level: Union[None, Level] = Level()
         self.clock = pygame.time.Clock()
         
         self.level.create(level_number = 1)
         self.player: Union[None, Player] = Player(100, HEIGHT - 100, 20, 40)
+
+        self.pine1_img = pygame.image.load('Background/pine1.png').convert_alpha()
+        self.pine2_img = pygame.image.load('Background/pine2.png').convert_alpha()
+        self.mountain_img = pygame.image.load('Background/mountain2.png').convert_alpha()
+        self.sky_cloud_img = pygame.image.load('Background/sky_cloud.png').convert_alpha()
+        self.sky_img = pygame.image.load('Background/sky_2.png').convert_alpha()
 
     def run(self):
         running = True
@@ -49,6 +56,8 @@ class Engine:
                 self.game()
             elif self.state == GameState.OPTIONS:
                 self.options()
+            elif self.state == GameState.EDITOR:
+                self.editor()
             else:
                 raise Exception(f"Invalid Game State: {self.state}")
 
@@ -56,6 +65,14 @@ class Engine:
     
     def draw_background(self):
         self.screen.fill((0, 0, 0))
+        width = self.sky_img.get_width()
+        for x in range(3):
+            self.screen.blit(self.sky_img, ((x * width), HEIGHT - 3*self.sky_img.get_height()))
+            self.screen.blit(self.sky_img, ((x * width), HEIGHT - 3 * self.sky_img.get_height() - 60))
+            self.screen.blit(self.sky_cloud_img, ((x * width), 0))
+            self.screen.blit(self.mountain_img, ((x * width), HEIGHT - self.mountain_img.get_height() - 400))
+            self.screen.blit(self.pine1_img, ((x * width), HEIGHT - self.pine1_img.get_height() - 350))
+            self.screen.blit(self.pine2_img, ((x * width), HEIGHT - self.pine2_img.get_height() - 300))
 
     def draw_game(self):
         self.draw_background()
@@ -64,6 +81,13 @@ class Engine:
         # draw GH
 
         pygame.display.update()
+
+    def draw_grid(self):
+        for col_num in range(COLS + 1):
+            pygame.draw.line(self.screen, (255, 255, 255), (col_num * TILE_WIDTH, 0), (col_num * TILE_WIDTH, HEIGHT))
+
+        for row_num in range(ROWS + 1):
+            pygame.draw.line(self.screen, (255, 255, 255), (0, row_num * TILE_HEIGHT), (WIDTH, row_num * TILE_HEIGHT))
 
     def menu(self):
         # Stuff
@@ -99,4 +123,12 @@ class Engine:
         # if choose == menu -> self.state = GameState.Menu
         raise NotImplemented()
 
-    
+    def editor(self):
+        if not self.resized:
+            self.screen = pygame.display.set_mode((WIDTH + SIDE_MARGIN, HEIGHT + LOWER_MARGIN))
+            self.resized = True
+        pygame.display.flip()
+        self.draw_background()
+        self.draw_grid()
+
+        pygame.display.update()
