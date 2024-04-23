ef9fff25df120a1ba089fa574634393ad2da488f
andresafira <andresafira2004@gmail.com>
2024-04-23 16:53:54 -0300

    feat: add final game state and one more level to test level update

--- src/levels/generator.py
+++ src/levels/generator.py
@@ -7,6 +7,7 @@ for i in range(6):
         map_[18 - i][5 + 3*i] = 0
 map_[17][24] = 0
 map_[16][24] = 0
-map_[15][24] = 0
-with open('l1.json', 'w') as file:
+map_[15][37] = 0
+map_[17][39] = -2
+with open('l2.json', 'w') as file:
     json.dump(map_, file, indent=2)
