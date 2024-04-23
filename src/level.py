e9e0a76c9e2aaee1796a063361c23d4fb10515ea
andresafira <andresafira2004@gmail.com>
2024-04-23 15:55:51 -0300

    feat: add goal detection

--- src/level.py
+++ src/level.py
@@ -3,15 +3,16 @@ from typing import Union
 from player import Player
 from geometry.vector import Vector
 from constants.game_constants import TILE_HEIGHT, TILE_WIDTH, FPS, LEVELS_PATH, N_LEVELS, HEIGHT, WIDTH, PIXEL_CORRECTION
+from constants.player_constants import HITPOINTS
 import pygame
 import json
 
 
 class Tile(Enum):
+    GOAL = -1
     EMPTY = 0
-    WALL = 1
-    GROUND = 2
-    # include other tile types
+    BLOCK = 1
+    DAMAGE = 2
 
 
 class Level:
@@ -32,7 +33,26 @@ class Level:
             return False
         return True
 
+    def update_player_state(self, player: Player) -> bool:
+        completed_level = False
+        for x in HITPOINTS['x']:
+            pos_x = player.position.x + x*player.width
+            j_pos = int(pos_x // TILE_WIDTH)
+            for y in HITPOINTS['y']:
+                pos_y = player.position.y - y*player.height
+                i_pos = int((HEIGHT - pos_y) // TILE_HEIGHT)
+
+                if self.map[i_pos][j_pos] == Tile.DAMAGE:
+                    player.alive = False
+                elif self.map[i_pos][j_pos] == Tile.GOAL:
+                    completed_level = True
+
+        return completed_level
+
     def simulate_move_point(self, position: Vector, velocity: Vector) -> tuple[bool, bool, Union[int, None], Union[int, None]]:
+        # Function that determines if the player is going to hit something on the next frame, and
+        # if so, updates if velocityand position, so it don't.
+        # returns xkeep, ykeep, xnew, ynew
         next_pos = position + velocity * (1 / FPS)
 
         i_next = int((HEIGHT - next_pos.y) // TILE_HEIGHT)
@@ -40,16 +60,16 @@ class Level:
         
         keep_Xspeed, keep_Yspeed = True, True
         new_x, new_y = None, None
-
-        if not self.is_valid(i_next, j_next) or self.map[i_next][j_next] != 0:
+        
+        if not self.is_valid(i_next, j_next) or self.map[i_next][j_next] != Tile.EMPTY:
             i_current = int((HEIGHT - position.y) // TILE_HEIGHT)
             j_current = int(position.x // TILE_WIDTH)
             
             updated = False
-            if not self.is_valid(i_next, j_current) or self.map[i_next][j_current] != 0:
+            if not self.is_valid(i_next, j_current) or self.map[i_next][j_current] != Tile.EMPTY:
                 keep_Yspeed = False
                 updated = True
-            if not self.is_valid(i_current, j_next) or self.map[i_current][j_next] != 0:
+            if not self.is_valid(i_current, j_next) or self.map[i_current][j_next] != Tile.EMPTY:
                 keep_Xspeed = False
                 updated = True
 
@@ -70,8 +90,8 @@ class Level:
         keep_Xspeed, keep_Yspeed = True, True
         new_x, new_y = None, None
         can_jump = False
-        for x in (0, 0.25, 0.5, 0.75, 1):
-            for y in (0, 0.25, 0.5, 0.75, 1):
+        for x in HITPOINTS['x']:
+            for y in HITPOINTS['y']:
                 if (not keep_Yspeed and not keep_Xspeed):
                     continue
                 corner_position = player.position + Vector(x*player.width, -y*player.height)
